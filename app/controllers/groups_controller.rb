160f7dce4da418871c0486f211fb17074d29d1f4
okutamatanuki <seinoazumi@gmail.com>
2019-07-13 17:50:03 +0900

    build group create function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,16 +2,28 @@ class GroupsController < ApplicationController
   def new
     @group = Group.new
     # @groupと関連しているusersの配列にcurrent_userを加える
+    # この下の行の記述がない場合、current_userのボックスにチェックが入らない
     @group.users << current_user
   end
 
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, {:user_ids => []})
   end
 
 end
