1aedc629a47c734eb3fb2baa2919a44c85a74493
okutamatanuki <seinoazumi@gmail.com>
2019-07-14 15:54:23 +0900

    build group edit function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,9 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
+  def index
+    @groups = Group.all
+  end
+
   def new
     @group = Group.new
     # @groupと関連しているusersの配列にcurrent_userを加える
@@ -19,11 +24,20 @@ class GroupsController < ApplicationController
   end
 
   def update
+    if @group.update(group_params)
+      redirect_to root_path, notice: 'グループ情報を編集しました'
+    else
+      render :edit
+    end
   end
 
   private
   def group_params
     params.require(:group).permit(:name, {:user_ids => []})
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 
 end
