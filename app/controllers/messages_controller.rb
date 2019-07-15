c8048c75c35c86708e9c874d294f4f691b19b0c4
okutamatanuki <seinoazumi@gmail.com>
2019-07-15 23:37:54 +0900

    build group function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,22 @@
 class MessagesController < ApplicationController
+  before_action :set_group
 
   def index
+    @message = @group.messages.new
   end
 
   def create
+    @message = @group.messages.new
+    if @message.save
+      redirect_to group_messages_path(@group)
+    end
   end
+
+  private
+
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
+
   
 end
