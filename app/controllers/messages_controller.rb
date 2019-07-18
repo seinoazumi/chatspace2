5a8197389ddb1b161968188fe98c1bc38b9ea07e
okutamatanuki <seinoazumi@gmail.com>
2019-07-18 20:15:28 +0900

    send message function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,12 +3,13 @@ class MessagesController < ApplicationController
 
   def index
     @message = @group.messages.new
+    @messages = @group.messages.all.includes(:user)
   end
 
   def create
-    @message = @group.messages.new
+    @message = @group.messages.new(message_params)
     if @message.save
-      redirect_to group_messages_path(@group)
+      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください。'
@@ -18,9 +19,12 @@ class MessagesController < ApplicationController
 
   private
 
+  def message_params
+    params.require(:message).permit(:text, :image).merge(user_id: current_user.id)
+  end
+
   def set_group
     @group = Group.find(params[:group_id])
   end
 
-  
 end
