77ff9b50405cf5adf7c310a5396d384153c4f838
okutamatanuki <seinoazumi@gmail.com>
2019-07-19 14:59:10 +0900

    push

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,4 +5,12 @@ class Message < ApplicationRecord
   validates :text, presence: true, unless: :image?
 
   mount_uploader :image, ImageUploader
+
+  def show_last_message
+    if(last_message = messages.last).present?
+      last_message.text? ? last_message.text : '画像が投稿されています'  
+    else
+      'まだメッセージはありません'
+    end
+  end
 end
