ff16dde245028c2e5d066b02254958a6e0ef3418
okutamatanuki <seinoazumi@gmail.com>
2019-07-19 20:57:52 +0900

    build message send function

--- app/models/message.rb
+++ app/models/message.rb
@@ -6,11 +6,4 @@ class Message < ApplicationRecord
 
   mount_uploader :image, ImageUploader
 
-  def show_last_message
-    if(last_message = messages.last).present?
-      last_message.text? ? last_message.text : '画像が投稿されています'  
-    else
-      'まだメッセージはありません'
-    end
-  end
 end
