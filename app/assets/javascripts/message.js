1b1d0caee19ec60b540665af827b02430352238d
okutamatanuki <seinoazumi@gmail.com>
2019-07-29 15:16:07 +0900

    revival of message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,23 +1,38 @@
 $(document).on('turbolinks:load', function(){
+  function buildMessage(message){
+    var imageUrl = message.image.url ? message.image.url : '';
+    var html = `<div class="message">
+                  <div class="upper-info">
+                    <p class="upper-info__user">${message.user_name}</p>
+                    <p class="upper-info__date">${message.date}</p>
+                  </div>
+                    <p class="message__text">${message.text}</p>
+                    <img src="${imageUrl}" class="message__text">
+                </div>`;
+    return html;
+  }
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     // FormDataの中にthisの中身を入れ、変数 formDataに代入
     var formData = new FormData(this);
     // thisの中のaction属性の中身をurlに代入
     var url = $(this).attr('action');
-
     $.ajax({
-      type: "POST",
       url: url,
+      type: "POST",
       data: formData,
-      dataType: 'json',
-      // ajaxに備わっているデータ成形機能をキャンセルする（formDataですでに整っているので）
+      dataType: 'json',  
       processData: false,
       contentType: false
     })
     .done(function(data){
       console.log(data);
-    
+      var html = buildMessage(data);
+      $('.messages').append(html);
+      $('.new-message')[0].reset();
+
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
     })
     .fail(function(){
       alert('メッセージの送信に失敗しました');
