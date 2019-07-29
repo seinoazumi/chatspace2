988a65dd2f9fa5469f32af1906a2a6210ecd45a5
okutamatanuki <seinoazumi@gmail.com>
2019-07-29 10:56:19 +0900

    edit form and controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   def index
-    
+    @users = User.where('name LIKE(?)', "%#{params[:search]}%").limit(20).where.not(id: current_user.id)
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 
   def edit
